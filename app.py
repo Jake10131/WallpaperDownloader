e265eb5a777b983573f45c2344f905bdd9363a02
Jake10131 <54140807+Jake10131@users.noreply.github.com>
2022-10-02 19:45:16 +0530

    Updated
    added a few changes.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as BSoup
-import random
 import os
+import datetime
 
 def hd_image(no, path):
  try:
@@ -9,8 +9,8 @@ def hd_image(no, path):
  except:
   pass
  os.chdir(os.path.join(os.getcwd(), path))
- pageNo = 0
- for x in range(no):
+ pageNo = 1
+ while (pageNo <= no):
   url = f'https://wall.alphacoders.com/featured.php?page={str(pageNo)}'
 
   headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"}
@@ -19,19 +19,20 @@ def hd_image(no, path):
 
   soup = BSoup(web.content, 'html.parser')
 
-  images = soup.find_all('img', class_="img-responsive big-thumb")
-  
-  pageNo = pageNo + 1
+  images = soup.find_all('img', class_="img-responsive")
   
   for image in images:
     link = image['src']
     name = image['alt']
-    img_name = random.randrange(1, 1000000)
+    iname = name.replace("|", "")
+    img_name = iname.replace(" ", "_")
     with open(str(img_name) + '.png', 'wb') as f:
             HdImg = requests.get(link)
             f.write(HdImg.content)
-            print(f"Saving:{name} PageNo:{pageNo}")
+            print(f"Saving:{iname} PageNo:{pageNo}")
+  pageNo += 1
             
 no_pages = int(input("Numbers of pages to scrape:\n"))
-folder = ('Images')
+time = datetime.datetime.now()
+folder = ('Images-'+str(time))
 hd_image(no_pages, folder)
